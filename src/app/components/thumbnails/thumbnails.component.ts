44f71a2e2c572b6fded396dc2591fdd9b3418dce
slarda <gianttiny123@gmail.com>
2019-09-07 08:45:51 -0400

    Update properties in some components

--- src/app/components/thumbnails/thumbnails.component.ts
+++ src/app/components/thumbnails/thumbnails.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThumbnailsComponent implements OnInit {
 
-  private params: any;
+  params: any;
 
   constructor() { }
 
--- src/app/components/title-link/title-link.component.ts
+++ src/app/components/title-link/title-link.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleLinkComponent implements OnInit {
 
-  private params: any;
+  params: any;
 
   constructor() { }
 
